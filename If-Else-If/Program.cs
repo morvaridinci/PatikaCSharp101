a38a1755b9ec57295ccf5c677835af4827250834
morvaridinci <77583188+morvaridinci@users.noreply.github.com>
2021-06-07 16:56:43 +0200

    If-Else-If

---
+++ If-Else-If/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace If_Else_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time =DateTime.Now.Hour;
+            if(time>=6 && time<=11){
+                Console.WriteLine("Gunaydin!");
+            }else if(time<=18){
+                Console.WriteLine("Iyi gunler!");
+            }else{
+                Console.WriteLine("Iyi geceler!");
+            }
+
+            string sonuc = time<=18 ? "Iyi gunler!" : "Iyi geceler!";
+            sonuc = time>=6 && time<11 ? "Gunaydin" : time<=18 ? "Iyi gunler"!: "Iyi geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
