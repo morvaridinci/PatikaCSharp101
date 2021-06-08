f1f2c68f99bcbac3e033a36a9a80c7c1833b3a4c
morvaridinci <77583188+morvaridinci@users.noreply.github.com>
2021-06-08 13:06:56 +0200

    While & Foreach

---
+++ While-Foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // 1'den başlayarak console'dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console'a yazdıran program.
+            Console.Write("Lütfen bir sayı girin:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                 toplam += sayac;
+                 sayac++;   
+            }
+            Console.WriteLine(toplam / sayi);
+
+            //'a' - 'z' ye kadar tüm harfleri console'a yazdır.
+            char character = 'a';
+            while (character <  'z')
+            {
+                 Console.Write(character);
+                 character++;    
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("***** Foreach *****");
+            string[] arabalar = {"BMV", "Ford", "Toyota", "Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
