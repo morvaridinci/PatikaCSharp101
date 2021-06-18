9afe52457d59ce4e5cf53f0ba866a49f589ff06a
morvaridinci <77583188+morvaridinci@users.noreply.github.com>
2021-06-18 14:54:18 +0200

    Sınıf Kavramı 1

---
+++ SinifKavrami1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sinif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayşe";
+            calisan1.Soyad = "Kara";
+            calisan1.No = 134678752;
+            calisan1.Departman = "İnsan Kaynakları";
+
+            calisan1.CalisanBiglileri();
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Deniz";
+            calisan2.Soyad = "Arda";
+            calisan2.No = 874123178;
+            calisan2.Departman = "Satın Alma";
+
+            calisan2.CalisanBiglileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBiglileri()
+        {
+            Console.WriteLine("Çalışan Adı: {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı: {0}",Soyad);
+            Console.WriteLine("Çalışan Numarası: {0}", No);
+            Console.WriteLine("Çalışanın Departmanı: {0}", Departman);
+        }
+    }
+}
