44d386b14515e64a6a28dbce80bd96f05fb1b9a5
morvaridinci <77583188+morvaridinci@users.noreply.github.com>
2021-06-18 15:02:43 +0200

    Kurucu Fonksiyonlar

---
+++ SinifKavrami2-KurucuFonksiyonlar/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan("Ayşe", "Kara", 23415634, "İnsan Kaynakları");
+
+            calisan1.CalisanBiglileri();
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Deniz";
+            calisan2.Soyad = "Arda";
+            calisan2.No = 25646789;
+            calisan2.Departman = "Satın Alma";
+
+            calisan2.CalisanBiglileri();
+
+            Calisan calisan3 = new Calisan("Zikriye", "Ürkmez");
+            calisan3.CalisanBiglileri();
+        }
+    }
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public Calisan(string ad, string soyad, int no, string departman)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            No = no;
+            Departman = departman;
+        }
+
+        public Calisan(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+        }
+
+        public Calisan(){}
+        
+        public void CalisanBiglileri()
+        {
+            Console.WriteLine("Çalışan Adı: {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı: {0}",Soyad);
+            Console.WriteLine("Çalışan Numarası: {0}", No);
+            Console.WriteLine("Çalışanın Departmanı: {0}", Departman);
+        }
+    }
+}
